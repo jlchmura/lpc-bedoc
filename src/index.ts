6e35550a654b20a1af0e385ab926df3bc2c4dcb1
jlchmura <jlchmura@gmail.com>
2025-02-13 19:15:40 -0500

    fix contract for printer

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const actions = [
     markdown.action,    
 ]
 
-const contract = `
+const contractParser = `
 ---
 provides:
   root:
@@ -38,5 +38,37 @@ provides:
           example:
             dataType: string[]
 `;
+const contractPrinter = `
+---
+accepts:
+  root:
+    dataType: object
+    contains:
+      functions:
+        dataType: object[]
+        contains:
+          name:
+            dataType: string
+          description:
+            dataType: string[]
+          param:
+            dataType: object[]
+            contains:
+              type:
+                dataType: string
+              name:
+                dataType: string
+              content:
+                dataType: string[]
+          return:
+            dataType: object
+            contains:
+              type:
+                dataType: string
+              content:
+                dataType: string[]
+          example:
+            dataType: string[]
+`;
 
-export const contracts = [contract, contract];
+export const contracts = [contractParser, contractPrinter];
